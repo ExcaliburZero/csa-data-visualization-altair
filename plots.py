271b63cd3157f8defd33180f29136cbd489f9e08
Christopher Wells <cwellsny@nycap.rr.com>
2018-05-21 03:25:46 -0400

    Add several more plots

--- plots.py
+++ plots.py
@@ -11,26 +11,52 @@ def main() -> None:
     """
     data = pd.read_csv(DATA_FILE)
 
-    attendance_chart = alt.Chart(data).mark_circle().encode(
-        x="Date:T",
-        y="Attendees",
-        color="Type",
-        tooltip="Meeting"
-    ).interactive()
+    brush = alt.selection(type="multi", encodings=['x'])
+
+    attendance_chart = (alt.Chart(data).mark_circle().encode(
+        alt.X("Date:T", timeUnit="month"),
+        y="count():O",
+        color="Year"
+    )).transform_filter(brush.ref()) + (alt.Chart(data).mark_line(
+            opacity=0.4
+        ).encode(
+        alt.X("Date:T", timeUnit="month"),
+        y="count():O",
+        color="Year"
+    )).transform_filter(brush.ref())
 
     yearly_summary = alt.Chart(data).mark_bar().encode(
         x="Year",
-        y="count()",
-        color="Type"
-    )
+        y="count()"
+    ).transform_filter(brush.ref())
 
-    monthly_summary = alt.Chart(data).mark_bar().encode(
+    attendance_monthly_summary = alt.Chart(data).mark_bar().encode(
         alt.X("Date:T", timeUnit="month"),
+        y="mean(Attendees)"
+    ).transform_filter(brush.ref())
+
+    attendance_yearly_summary = alt.Chart(data).mark_bar().encode(
+        x="Year",
+        y="mean(Attendees):Q"
+    ).transform_filter(brush.ref())
+
+    attendance_histogram = alt.Chart(data).mark_bar().encode(
+        alt.X("Attendees:Q", bin=True),
         y="count()",
-        color="Type"
+        color="Type",
+        tooltip="Meeting"
+    ).transform_filter(brush.ref())
+
+    type_by_year = alt.Chart(data).mark_bar().encode(
+        x="Type:N",
+        y="count()",
+        color="Year",
+        opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.4))
+    ).properties(
+        selection=brush
     )
 
-    charts = (attendance_chart | yearly_summary) & (monthly_summary)
+    charts = ((attendance_chart | yearly_summary | attendance_histogram) & (attendance_monthly_summary | attendance_yearly_summary | type_by_year))
 
     charts.save(OUTPUT_FILE)
 
